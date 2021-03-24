e9349e05b9d18188bc651b27ad4a1b2fe839d941
oussamarh96 <73478930+oussamarh96@users.noreply.github.com>
2021-03-24 14:44:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,12 @@
-
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:myapp/pages/login.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/home.dart';
-import 'package:myapp/pages/login.dart';
+
 
 void main(){
 
   runApp(MaterialApp(
     home: HomePage(),
   ));
-}
-
-// hedhi ta3mel intialisation mta3 firebase
-class Appp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire
-      future: Firebase.initializeApp(),
-      builder: (context, snapshot) {
-        // Check for errors
-        
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Login();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return HomePage();
-      },
-    );
-  }
 }
